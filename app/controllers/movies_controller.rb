9f22fb854529870697be1e98c73e577197480bae
Sol Patel <solly.p@gmail.com>
2016-12-09 03:37:50 +0000

    addeded validations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,25 +15,49 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
   
-  def new
-    
-  end
+##### OLD CREATE METHOD  
+#  def create
+#    @movie = Movie.create!(movie_params)
+#    flash[:notice] = "#{@movie.title} was successfully created."
+#    redirect_to movies_path
+#  end
   
   def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    @movie = Movie.new(params[:movie])
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    else
+      render 'new' # note, 'new' template can access @movie's field values!
+    end 
+  end
+
+### OLD UPDATE
+#  def update
+#    @movie = Movie.find params[:id]
+#    @movie.update_attributes(movie_params)
+#   flash[:notice] = "#{@movie.title} was successfully updated."
+#    redirect_to movies_path
+#  end 
+
+# replaces the 'update' method in controller:
+  def update
+    @movie = Movie.find params[:id]
+    if @movie.update_attributes(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+    else
+      render 'edit' # note, 'edit' template can access @movie's field values!
+    end
+  end
+
+# as a reminder, here is the original 'new' method:
+  def new
+    @movie = Movie.new
   end
   
   def edit
     @movie = Movie.find params[:id]
-  end 
-
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movies_path
   end 
   
   def destroy
