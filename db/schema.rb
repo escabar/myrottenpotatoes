6c357c6b66c354e2c0883cec5ff5e61888510747
Sol Patel <solly.p@gmail.com>
2016-12-29 23:15:17 +0000

    added moviegoers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114035945) do
+ActiveRecord::Schema.define(version: 20161229230845) do
+
+  create_table "moviegoers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
